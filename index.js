2f5b5bf42f6280f12b5b1e901ad44ae7dcef2f53
Kristi <teneqexhi.kristi@gmail.com>
2026-01-06 21:49:30 +0100

    Added draft 1 functioning darkmode toggel button and new css sheet

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ darkMode.onclick = function(){
     console.log(darkMode.textContent);
     if(darkMode.textContent =='Dark Mode'){
         darkMode.textContent = 'Light Mode';
+        console.log(document.getElementById("style"));
+        document.getElementById("style").href = `./styles/style_dark.css`
     }else{
         darkMode.textContent = 'Dark Mode';
+        document.getElementById("style").href = `./styles/style.css`
     }
 }
 
