437cd9986fca70b286f73ee8eae74131c4e8ac7f
Kristi <teneqexhi.kristi@gmail.com>
2026-01-08 21:23:26 +0100

    Added memory across pages for darkmode

--- index.js
+++ index.js
@@ -1,17 +1,88 @@
-let username;
+let username
 
-let darkMode = document.getElementById("toggle_mode")
-darkMode.onclick = function(){
-    console.log(darkMode.textContent);
-    if(darkMode.textContent =='Dark Mode'){
-        darkMode.textContent = 'Light Mode';
+let button_mode;
+
+let darkColorMode = localStorage.getItem("darkMode");
+
+button_mode = document.getElementById("toggle_mode");
+
+console.log(darkColorMode);
+if(darkColorMode === null){
+    darkColorMode = (button_mode.textContent != 'Dark Mode')? '1' : '0';
+}
+
+//in case ne cookie eshte ruajtur darkmode
+if(darkColorMode=='1'){
+    turnBlack();
+}else{
+     //defaulti already eshte white mode 
+}
+
+function turnBlack(){
+    button_mode.textContent = 'Light Mode';
+    document.getElementById("style").href = `./styles/style_dark.css`
+}
+function turnWhite(){
+    button_mode.textContent = 'Dark Mode';
+    document.getElementById("style").href = `./styles/style.css`
+}
+
+button_mode.onclick = function(){
+    console.log(darkColorMode);
+    if(darkColorMode=='1'){
+        localStorage.setItem("darkMode", '0');
+        turnWhite();    
+    }else{
+        localStorage.setItem("darkMode", '1');
+        turnBlack();    
+    }
+    darkColorMode = localStorage.getItem("darkMode");
+    console.log(darkColorMode);
+
+}
+/*let button_mode;
+
+let darkColorMode = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith("colorMode="))
+    ?.split("=")[1];
+
+button_mode = document.getElementById("toggle_mode");
+
+console.log(darkColorMode);
+if(darkColorMode === undefined){
+    darkColorMode = (button_mode.textContent=='Dark Mode')? false:true;
+}
+//in case ne cookie eshte ruajtur darkmode
+if(darkColorMode=='true'){
+    document.cookie = 'colorMode=true;'
+    button_mode.textContent = 'Light Mode';
+    console.log(document.getElementById("style"));
+    document.getElementById("style").href = `./styles/style_dark.css`
+}
+console.log(darkColorMode);
+
+button_mode.onclick = function(){
+    console.log(darkColorMode);
+
+    
+    console.log(darkColorMode);
+    if(darkColorMode=='true'){
+        document.cookie = 'colorMode=flase;'
+        button_mode.textContent = 'Light Mode';
         console.log(document.getElementById("style"));
         document.getElementById("style").href = `./styles/style_dark.css`
     }else{
-        darkMode.textContent = 'Dark Mode';
+        document.cookie = 'colorMode=true;'
+        button_mode.textContent = 'Dark Mode';
         document.getElementById("style").href = `./styles/style.css`
     }
-}
+    
+    darkColorMode = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith("colorMode="))
+    ?.split("=")[1];
+}*/
 
 const cookieValue = document.cookie
 .split("; ")
--- workspace.js
+++ workspace.js
@@ -1,4 +1,46 @@
+let button_mode;
 
+let darkColorMode = localStorage.getItem("darkMode");
+
+button_mode = document.getElementById("toggle_mode");
+
+console.log(darkColorMode);
+if(darkColorMode === null){
+    darkColorMode = (button_mode.textContent != 'Dark Mode')? '1' : '0';
+}
+
+//in case ne cookie eshte ruajtur darkmode
+if(darkColorMode=='1'){
+    turnBlack();
+}else{
+     //defaulti already eshte white mode 
+}
+
+function turnBlack(){
+    button_mode.textContent = 'Light Mode';
+    document.getElementById("style").href = `../styles/style_dark.css`
+}
+function turnWhite(){
+    button_mode.textContent = 'Dark Mode';
+    document.getElementById("style").href = `../styles/style.css`
+}
+
+button_mode.onclick = function(){
+    console.log(darkColorMode);
+    if(darkColorMode=='1'){
+        localStorage.setItem("darkMode", '0');
+        turnWhite();    
+    }else{
+        localStorage.setItem("darkMode", '1');
+        turnBlack();    
+    }
+    darkColorMode = localStorage.getItem("darkMode");
+    console.log(darkColorMode);
+
+}
+
+
+/*
 let darkMode = document.getElementById("toggle_mode")
 darkMode.onclick = function(){
     console.log(darkMode.textContent);
@@ -10,4 +52,4 @@ darkMode.onclick = function(){
         darkMode.textContent = 'Dark Mode';
         document.getElementById("style").href = `../styles/style.css`
     }
-}
+}*/
